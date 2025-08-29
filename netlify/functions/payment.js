63892465c27963e65e416159b5f9e4bec8cc735e
Chenlong <aa2231401652@gmail>
2025-08-29 16:19:33 +0800

    添加支付宝回调

--- netlify/functions/payment.js
+++ netlify/functions/payment.js
@@ -68,7 +68,7 @@ exports.handler = async (event) => {
                 out_trade_no: outTradeNo,
                 total_amount: price,
                 subject: subject,
-                notify_url: 'https://mediamingle.cn/.netlify/functions/alipay-notify'
+                notify_url: '/.netlify/functions/alipay-notify'
             },
         });
 
